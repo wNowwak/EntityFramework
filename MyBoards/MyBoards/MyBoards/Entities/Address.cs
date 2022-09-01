872a6e5cc2a730b83b0c395448bee0bf1a3b47d8
wojtek <nowwak.wojtek@gmail.com>
2022-09-01 21:13:59 +0200

    Added classes for entities

---
+++ MyBoards/MyBoards/MyBoards/Entities/Address.cs
@@ -0,0 +1,10 @@
+﻿namespace MyBoards.Entities
+{
+    public class Address
+    {
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
